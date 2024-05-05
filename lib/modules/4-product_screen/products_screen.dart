f72ca68f876251fd9b3effa7086104405fa1732a
Ahmed Shosha <ashosha763@gmail.com>
2024-05-06 00:56:31 +0300

    view filter in products_screen.dart

--- lib/modules/4-product_screen/products_screen.dart
+++ lib/modules/4-product_screen/products_screen.dart
@@ -31,196 +31,217 @@ class _ProductsScreenState extends State<ProductsScreen> {
               children: [
                 Padding(
                   padding: const EdgeInsets.only( left: 40.0, right: 40, top: 30, bottom: 10),
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        width: 570,
-                        height: 48,
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                              filled: true,
-                              fillColor: HexColor(white),
-                              hintText: 'Search product by name',
-                              hintStyle: const TextStyle(
-                                color: Color(0xffb2bac6),
-                                fontFamily: 'Poppins',
-                              ),
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: HexColor(bWhite90),
-                                size: 21,
-                              ),
-                              border: const OutlineInputBorder(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(12),
-                                  topLeft: Radius.circular(12),
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: HexColor(bWhite90)),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: HexColor(primary)),
-                              ),
-                              contentPadding:
+                  child: Stack(
+                    children:[
+                      const Column(
+                        children: [
+                          SizedBox(
+                            height: 60,
+                          ),
+                          TableWidget(),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: 570,
+                            height: 48,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  filled: true,
+                                  fillColor: HexColor(white),
+                                  hintText: 'Search product by name',
+                                  hintStyle: const TextStyle(
+                                    color: Color(0xffb2bac6),
+                                    fontFamily: 'Poppins',
+                                  ),
+                                  prefixIcon: Icon(
+                                    Icons.search,
+                                    color: HexColor(bWhite90),
+                                    size: 21,
+                                  ),
+                                  border: const OutlineInputBorder(
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(12),
+                                      topLeft: Radius.circular(12),
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: HexColor(bWhite90)),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(color: HexColor(primary)),
+                                  ),
+                                  contentPadding:
                                   const EdgeInsets.symmetric(vertical: 5)),
-                        ),
-                      ),
-                      const SizedBox(width: 75),
-                      MaterialButton(
-                        onPressed: () {
-                          setState(() {
-                            filterVisibility = !filterVisibility;
-                          });
-                        },
-                        color: HexColor('#edf0fe'),
-                        height: 48,
-                        minWidth: 105,
-                        elevation: 0,
-                        hoverColor: Colors.transparent,
-                        shape: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: HexColor('#edf0fe')
+                            ),
                           ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child:
-                          Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Row(
-                                children: [
-                                  setPhoto(kind: 1,path: 'assets/icons/filter.svg'),
-                                  const SizedBox(width: 10),
-                                  Text('Filter', style: TextStyle(
-                                    color: HexColor(primary),
-                                    fontFamily: 'Poppins',
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 16
-                                  ),
-                                  ),
-                                ],
+                          const SizedBox(width: 75),
+                          MaterialButton(
+                            onPressed: () {
+                              setState(() {
+                                filterVisibility = !filterVisibility;
+                              });
+                            },
+                            color: HexColor('#edf0fe'),
+                            height: 48,
+                            minWidth: 105,
+                            elevation: 0,
+                            hoverColor: Colors.transparent,
+                            shape: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: HexColor('#edf0fe')
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child:
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: [
+                                    setPhoto(kind: 1,path: 'assets/icons/filter.svg'),
+                                    const SizedBox(width: 10),
+                                    Text('Filter', style: TextStyle(
+                                        color: HexColor(primary),
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16
+                                    ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                      ),
-                      const SizedBox(width: 10),
-                      MaterialButton(
-                        onPressed: () {},
-                        color: HexColor('#edf0fe'),
-                        height: 48,
-                        minWidth: 105,
-                        elevation: 0,
-                        hoverColor: Colors.transparent,
-                        shape: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: HexColor('#edf0fe')
+                          const SizedBox(width: 10),
+                          MaterialButton(
+                            onPressed: () {},
+                            color: HexColor('#edf0fe'),
+                            height: 48,
+                            minWidth: 105,
+                            elevation: 0,
+                            hoverColor: Colors.transparent,
+                            shape: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: HexColor('#edf0fe')
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child:
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Row(
+                                  children: [
+                                    setPhoto(kind: 1,path: 'assets/icons/category.svg'),
+                                    const SizedBox(width: 10),
+                                    Text('Categories', style: TextStyle(
+                                        color: HexColor(primary),
+                                        fontFamily: 'Poppins',
+                                        fontWeight: FontWeight.w500,
+                                        fontSize: 16
+                                    ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child:
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Row(
-                              children: [
-                                setPhoto(kind: 1,path: 'assets/icons/category.svg'),
-                                const SizedBox(width: 10),
-                                Text('Categories', style: TextStyle(
-                                    color: HexColor(primary),
+                          const SizedBox(width: 10),
+                          MaterialButton(
+                            onPressed: () {},
+                            color: HexColor(primary),
+                            height: 48,
+                            minWidth: 105,
+                            elevation: 0,
+                            hoverColor: Colors.transparent,
+                            shape: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: HexColor(primary)
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child:
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text('Add Product', style: TextStyle(
+                                    color: HexColor(white),
                                     fontFamily: 'Poppins',
                                     fontWeight: FontWeight.w500,
                                     fontSize: 16
                                 ),
                                 ),
-                              ],
+                              ),
                             ),
                           ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 50.0,right: 410),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Visibility(
+                              visible: filterVisibility,
+                              child: Container(
+                                margin: const EdgeInsets.only(top: 5),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(8.0)),
+                                width: 280,
+                                height: 227,
+                                child: const Padding(
+                                  padding: EdgeInsets.only(right: 20.0),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(left: 20.0, top: 20.0),
+                                        child: Text(
+                                          "Filter Options",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: 'Poppins-SemiBold',
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 14.0),
+                                        child: Divider(
+                                          height: 1,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(top: 20.0, left: 20.0),
+                                        child: Text(
+                                          "State:",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: 'Poppins-Medium',
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(width: 10),
-                      MaterialButton(
-                        onPressed: () {},
-                        color: HexColor(primary),
-                        height: 48,
-                        minWidth: 105,
-                        elevation: 0,
-                        hoverColor: Colors.transparent,
-                        shape: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: HexColor(primary)
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child:
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text('Add Product', style: TextStyle(
-                                color: HexColor(white),
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16
-                            ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+                    ]
                   ),
                 ),
-                const TableWidget(),
+
               ],
             ),
           ),
         ),
-        Visibility(
-          visible: filterVisibility,
-          child: Container(
-            margin: const EdgeInsets.only(top: 5),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(8.0)),
-            width: 280,
-            height: 227,
-            child: const Padding(
-              padding: EdgeInsets.only(right: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(left: 20.0, top: 20.0),
-                    child: Text(
-                      "Filter Options",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Poppins-SemiBold',
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 14.0),
-                    child: Divider(
-                      height: 1,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 20.0, left: 20.0),
-                    child: Text(
-                      "State:",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'Poppins-Medium',
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        )
+
       ],
     );
   }
